b25933b14b17b47eea1e507e2ab36154d375e292
Kaardal88 <123365145+Kaardal88@users.noreply.github.com>
2024-09-23 19:55:51 +0200

    improvements on card style

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const readMore = document.getElementById("read-more-btn");
-const moreText = document.getElementById("more-text");
-
-readMore.addEventListener('click', () => {
-    if (moreText.style.display === 'none' || moreText.style.display === '') {
-        moreText.style.display = 'inline';
-        readMore.textContent = 'Read Less';
-    } else {
-        moreText.style.display = 'none';
-        readMore.textContent = 'Read More';
-    }
-})
+document.querySelectorAll('.read-more-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        const moreText = button.previousElementSibling.querySelector('.more-text');
+        if (moreText.style.display === 'none' || moreText.style.display === '') {
+            moreText.style.display = 'inline';
+            button.textContent = 'Read Less';
+        } else {
+            moreText.style.display = 'none';
+            button.textContent = 'Read More';
+        }
+    });
+});
